cb14c1dc9256d0de8061f1e217e53a7d189c60f9
Anas <anas.zulkifli2001@gmail.com>
2025-08-28 23:39:45 +0800

    feat: data

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Github, Linkedin } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
@@ -9,6 +10,28 @@ export default function Home() {
       <div >
         <h1 className="text-center">Anas Zulkifli</h1>
         <h2 className="text-center">Mobile Developer</h2>
+        <div className="flex justify-center">
+          <Linkedin />
+          <Github />
+        </div>
+
+
+      </div>
+      <div className="grid grid-cols-3">
+        <div className="flex flex-col items-center text-center">
+          <h3>2</h3>
+          <h3>Years of work experience</h3>
+        </div>
+        <div className="flex flex-col items-center text-center">
+          <h3>3</h3>
+          <h3>Completed projects</h3>
+
+        </div>
+        <div className="flex flex-col items-center text-center">
+          <h3>5 +</h3>
+          <h3>Personal side projects</h3>
+        </div>
+
 
       </div>
 
