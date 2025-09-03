8babec98fb553e9056112229da0a98807744709a
Anas <anas.zulkifli2001@gmail.com>
2025-09-03 22:08:46 +0800

    feat: update UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+import { AspectRatio } from "@/components/ui/aspect-ratio";
 import { Button } from "@/components/ui/button";
+import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Github, Linkedin } from "lucide-react";
 import Image from "next/image";
@@ -38,7 +40,7 @@ export default function Home() {
       </div>
       <div className="flex gap-x-2">
         <Button className="flex-1">Download CV</Button>
-        <Button className="flex-1">Download CV</Button>
+        <Button variant="outline" className="flex-1  border-foreground">Contact Me</Button>
       </div>
 
       <Tabs defaultValue="account" >
@@ -47,7 +49,20 @@ export default function Home() {
           <TabsTrigger value="password">Password</TabsTrigger>
         </TabsList>
         <TabsContent value="account"><div className="grid grid-cols-1">
-          <Image src="/aonic-agri-1.jpg" alt="Vercel Logo" width={400} height={400} className="text-center w-3/5" />
+          <Card className="my-2">
+            <CardHeader>
+              <CardTitle>Aonic Agriculture App</CardTitle>
+              <CardDescription>All-in-one farming companion — browse agrochemical products, shop online or in-store, redeem vouchers, and find nearby branches with ease.</CardDescription>
+            </CardHeader>
+
+          </Card>
+          <Card className="my-2">
+            <CardHeader>
+              <CardTitle>IseBa</CardTitle>
+              <CardDescription>Designed to make learning Malay simple and engaging — practice spelling, explore syllables, and perfect your pronunciation step by step.</CardDescription>
+            </CardHeader>
+
+          </Card>
         </div></TabsContent>
         <TabsContent value="password">Change your password here.</TabsContent>
       </Tabs>
